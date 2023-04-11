bead373b12a01ddb4eaf1f8680e8f83c8be6c7a3
genkhall <argenargen0805@gmail.com>
2023-04-11 17:01:05 +0600

    выполнено

--- main.py
+++ main.py
@@ -93,6 +93,8 @@ async def echo(message: types.Message):
         await message.answer(message.text)
 
 
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
